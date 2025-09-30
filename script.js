3053d39071836ef71622f9bf51e71369d2c02fcd
tavaresx1912 <gui.tavaresx@gmail.com>
2025-09-30 20:12:16 -0300

    Integrate Bootstrap framework and add form functionalities
    - Updated `index.html` with Bootstrap components and layout.
    - Added basic styles for the container in `styles.css`.
    - Created `script.js` with input event handling and alerts.
    - Removed `READ.ME` as it is no longer needed.

---
+++ script.js
@@ -0,0 +1,10 @@
+const input_usário = document.getElementById('input_usário');
+
+alert('Bem-vindo ao site!');
+
+input_usário.addEventListener('input', function() {
+    alert('O usuário digitou: ' + input_usário.value);
+});
+
+# USUÁRIO ESCOLHEU A OPÇAÕ : X
+# ELE ESCREVEU : Y
